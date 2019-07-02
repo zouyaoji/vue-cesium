77c7477e75ab32d6387f8ace021136921494a8ef
zouyaoji <370681295@qq.com>
2019-07-02 16:02:05 +0800

    fixed #9

--- src/util/bindEvent.js
+++ src/util/bindEvent.js
@@ -3,12 +3,10 @@ import { toKebabCase } from './util.js'
 
 export default function (instance, eventList) {
   const ev = eventList || Events[toKebabCase(this.$options._componentTag)]
-  ev && ev.forEach(event => {
-    if (instance[event]) {
-      const hasOn = event.slice(0, 2) === 'on'
-      const eventName = hasOn ? event.slice(2) : event
+  ev && ev.forEach(eventName => {
+    if (instance[eventName]) {
       const listener = this.$listeners[eventName]
-      listener && instance[event].addEventListener(listener.fns)
+      listener && instance[eventName].addEventListener(listener.fns)
     } else {
       // console.log('Add event linstener of ' + event + ' failed, try to upgrade Cesium to latest version.')
     }
