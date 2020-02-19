94845421777737e152f0f073a61df0bf71082d41
zouyaoji <370681295@qq.com>
2020-02-19 16:25:58 +0800

    added

---
+++ src/components/primitive/particle/ParticleSystem.vue
@@ -0,0 +1,47 @@
+<script>
+import {
+  show,
+  modelMatrix,
+  image
+} from '../../../mixins/mixinProps'
+import mixinPrimitive from '../../../mixins/primitives/mixinPrimitive'
+export default {
+  name: 'vc-primitive-particle',
+  mixins: [
+    show,
+    modelMatrix,
+    image,
+    mixinPrimitive
+  ],
+  props: {
+    color: Object | String | Array,
+    updateCallback: Function,
+    emitter: Object,
+    emitterModelMatrix: Object,
+    emissionRate: Number,
+    bursts: Array,
+    loop: {
+      type: Boolean,
+      default: true
+    },
+    scale: Number,
+    startScale: Number,
+    endScale: Number,
+    startColor: Object | String | Array,
+    endColor: Object | String | Array,
+    imageSize: Object,
+    minimumImageSize: Object,
+    maximumImageSize: Object,
+    speed: Number,
+    minimumSpeed: Number,
+    maximumSpeed: Number,
+    lifetime: Number,
+    particleLife: Number,
+    minimumParticleLife: Number,
+    maximumParticleLife: Number,
+    mass: Number,
+    minimumMass: Number,
+    maximumMass: Number
+  }
+}
+</script>
