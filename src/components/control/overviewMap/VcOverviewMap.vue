d2dade776e4d16071fd76a9d7925f2f86be41953
zouyaoji <370681295@qq.com>
2020-08-14 18:47:26 +0800

    完善鹰眼控件

--- src/components/control/overviewMap/VcOverviewMap.vue
+++ src/components/control/overviewMap/VcOverviewMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="leaflet-control-minimap" id="overview" ref="leafletContainer"></div>
+  <div :style="ovStyle" class="leaflet-control-minimap" id="vc-overview-map" ref="leafletContainer"></div>
 </template>
 
 <script>
@@ -12,20 +12,66 @@ export default {
   name: 'vc-map-overview',
   mixins: [cmp],
   props: {
+    url: {
+      type: String,
+      default: 'https://webst01.is.autonavi.com/appmaptile?style=7&x={x}&y={y}&z={z}'
+    },
+    height: {
+      type: Number,
+      default: 150
+    },
+    width: {
+      type: Number,
+      default: 150
+    },
+    anchor: {
+      type: String,
+      default: 'bottomright'
+    }
   },
   data () {
     return {
-      nowaiting: true
+      nowaiting: true,
+      bottom: 10,
+      right: 10,
+      top: 50,
+      left: 10
     }
   },
-  mounted () {
-    this.$parent.createPromise.then(({ Cesium, viewer }) => {
+  computed: {
+    ovStyle: function () {
+      const { anchor, height, width, top, left, right, bottom } = this
+      const style = {
+        height: height + 'px',
+        width: width + 'px'
+      }
 
-    })
+      if (anchor === 'topleft') {
+        style.top = top + 'px'
+        style.left = left + 'px'
+      }
+
+      if (anchor === 'topright') {
+        style.top = top + 'px'
+        style.right = right + 'px'
+      }
+
+      if (anchor === 'bottomright') {
+        style.bottom = bottom + 'px'
+        style.right = right + 'px'
+      }
+
+      if (anchor === 'bottomleft') {
+        style.bottom = bottom + 'px'
+        style.left = left + 'px'
+      }
+
+      return style
+    }
   },
   methods: {
     async createCesiumObject () {
-      const { viewer } = this
+      const { viewer, width, height, anchor } = this
       var url = 'https://webst01.is.autonavi.com/appmaptile?style=7&x={x}&y={y}&z={z}'
       var layer = new L.TileLayer(url, {
         minZoom: 0,
@@ -35,9 +81,9 @@ export default {
       var options = {
         container: container,
         toggleDisplay: true,
-        width: 150,
-        height: 150,
-        position: 'topright',
+        width: width,
+        height: height,
+        position: anchor,
         aimingRectOptions: {
           color: '#ff1100',
           weight: 3
@@ -49,12 +95,58 @@ export default {
           fillOpacity: 0
         }
       }
+      viewer.widgetResized.addEventListener(this.widgetResized)
+      this.widgetResized()
       return new CesiumOverviewMapControl(viewer, layer, options)
+    },
+    widgetResized () {
+      let bottom = 10
+      let right = 10
+      const { viewer, anchor } = this
+      this.$nextTick(() => {
+        if (anchor === 'bottomright') {
+          if (viewer.timeline) {
+            bottom += viewer.timeline.container.getBoundingClientRect().height
+          }
+          const vcDistance = document.querySelector('.vc-location-distance')
+          if (vcDistance) {
+            bottom += vcDistance.getBoundingClientRect().height
+          }
+        } else if (anchor === 'bottomleft') {
+          if (viewer.animation) {
+            bottom += viewer.animation.container.getBoundingClientRect().height
+          } else {
+            if (viewer.bottomContainer) {
+              bottom += viewer.bottomContainer.getBoundingClientRect().height
+            }
+            if (viewer.timeline) {
+              bottom += viewer.timeline.container.getBoundingClientRect().height
+            }
+          }
+        }
+
+        if (anchor === 'topright') {
+          const navigationNavs = document.querySelector('.vc-navigation-navs')
+          if (navigationNavs) {
+            right += navigationNavs.getBoundingClientRect().width + 16
+          }
+        }
+
+        if (bottom === 10) {
+          let right = 10
+          viewer.fullscreenButton && (right += viewer.fullscreenButton.container.getBoundingClientRect().width)
+          viewer.vrButton && (right += viewer.vrButton.container.getBoundingClientRect().width)
+        }
+
+        this.bottom = bottom
+        this.right = right
+      })
     },
     async mount () {
       return true
     },
     async unmount () {
+      this.viewer.widgetResized.removeEventListener(this.widgetResized)
       return true
     }
   },
