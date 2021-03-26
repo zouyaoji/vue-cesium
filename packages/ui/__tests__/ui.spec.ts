daee1ce5426a8469dd59d398439be84c0358127d
zouyaoji <370681295@qq.com>
2021-03-27 01:58:34 +0800

    feat: 🎸 Added basic UI components

---
+++ packages/ui/__tests__/ui.spec.ts
@@ -0,0 +1,15 @@
+// import { mount } from '@vue/test-utils'
+// import Ui from '../src/index.vue'
+
+// const AXIOM = 'LML is the best girl'
+
+// describe('Ui.vue', () => {
+//   test('render test', () => {
+//     const wrapper = mount(Ui, {
+//       slots: {
+//         default: AXIOM,
+//       },
+//     })
+//     expect(wrapper.text()).toEqual(AXIOM)
+//   })
+// })
---
+++ packages/ui/spinner/use-spinner.ts
@@ -0,0 +1,18 @@
+import { computed } from 'vue'
+import { useSizeDefaults } from '@vue-cesium/composables/private/use-size'
+
+export const useSpinnerProps = {
+  size: {
+    type: [Number, String],
+    default: '1em'
+  },
+  color: String
+}
+
+export default function useSpinner(props) {
+  return {
+    cSize: computed(() => (props.size in useSizeDefaults ? `${useSizeDefaults[props.size]}px` : props.size)),
+
+    classes: computed(() => 'vc-spinner' + (props.color ? ` text-${props.color}` : ''))
+  }
+}
