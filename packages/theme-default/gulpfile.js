b59773feb41cc9745272829ac81e50ebde8d6daa
zouyaoji <370681295@qq.com>
2021-03-27 01:56:20 +0800

    style: 💄 Added default style files

---
+++ packages/theme-default/gulpfile.js
@@ -0,0 +1,30 @@
+'use strict'
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { series, src, dest } = require('gulp')
+const sass = require('gulp-dart-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cssmin = require('gulp-cssmin')
+const rename = require('gulp-rename')
+
+const noElPrefixFile = /(index|base)/
+
+function compile() {
+  return src('./src/*.scss')
+    .pipe(sass.sync())
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(cssmin())
+    .pipe(rename(function (path) {
+      if(!noElPrefixFile.test(path.basename)) {
+        path.basename = `vc-${path.basename}`
+      }
+    }))
+    .pipe(dest('./lib'))
+}
+
+function copyfont() {
+  return src('./src/fonts/**')
+    .pipe(cssmin())
+    .pipe(dest('./lib/fonts'))
+}
+
+exports.build = series(compile, copyfont)
