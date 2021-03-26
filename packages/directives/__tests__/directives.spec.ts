67ce0b52874bac03eec56258a791e99befa15379
zouyaoji <370681295@qq.com>
2021-03-27 01:59:31 +0800

    feat: 🎸 Added ripple directive

---
+++ packages/directives/__tests__/directives.spec.ts
@@ -0,0 +1,15 @@
+// import { mount } from '@vue/test-utils'
+// import Directives from '../src/index.vue'
+
+// const AXIOM = 'LML is the best girl'
+
+// describe('Directives.vue', () => {
+//   test('render test', () => {
+//     const wrapper = mount(Directives, {
+//       slots: {
+//         default: AXIOM,
+//       },
+//     })
+//     expect(wrapper.text()).toEqual(AXIOM)
+//   })
+// })
