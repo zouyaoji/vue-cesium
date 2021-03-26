9ac6f3a9f926c5facc74d73e6f3b7141382fff08
zouyaoji <370681295@qq.com>
2021-03-27 02:07:47 +0800

    feat: 🎸 Added locale of vue-cesium

---
+++ packages/locale/index.ts
@@ -0,0 +1,53 @@
+import defaultLang from './lang/zh-hans'
+
+export type TranslatePair = {
+  [key: string]: string | string[] | TranslatePair
+}
+
+export type Language = {
+  name: string,
+  nativeName: string
+  vc: TranslatePair
+}
+
+let lang: Language = defaultLang as Language
+
+let i18nHandler: null | ((...args: any[]) => string) = null
+
+export const i18n = (fn: (...args: any[]) => string) => {
+  i18nHandler = fn
+}
+
+function template(str: string, option) {
+  if(!str || !option) return str
+
+  return str.replace(/\{(\w+)\}/g, (match, key) => {
+    return option[key]
+  })
+}
+
+export const t = (...args: any[]): string => {
+  if (i18nHandler) return i18nHandler(...args)
+
+  const [path, option] = args
+  let value
+  const array = path.split('.')
+  let current = lang
+  for (let i = 0, j = array.length; i < j; i++) {
+    const property = array[i]
+    value = current[property]
+    if (i === j - 1) return template(value, option)
+    if (!value) return ''
+    current = value
+  }
+  return ''
+}
+
+export const use = (l: Language): void => {
+  lang = l || lang
+  if (lang.name) {
+    // dayjs.locale(lang.name)
+  }
+}
+
+export default { use, t, i18n }
---
+++ packages/locale/lang/en-us.ts
@@ -0,0 +1,68 @@
+export default {
+  name: 'en-us',
+  nativeName: 'English (US)',
+  vc: {
+    loadError: 'needs to be child of VcViewer',
+    measure: {
+      distance: 'Distance',
+      area: 'Area',
+      horizontalDistance: 'HD',
+      spaceDistance: 'SD',
+      verticalHeight: 'VH'
+    },
+    draw: {
+      drawingTip1: 'Click the left button to draw the starting point.',
+      drawingTip2: 'Click the left button to draw the next point, and click the right button to end the drawing.',
+      drawingTip3: 'Click to end editing.',
+      editingMove: 'Move',
+      editingInsert: 'Insert',
+      editingDelete: 'Delete'
+    },
+    navigation: {
+      compass: {
+        outerTip: 'Drag outer ring: rotate view. Double-click: reset view.',
+        innerTip: 'Drag inner gyroscope: free orbit. TIP: You can also free orbit by holding the CTRL key and dragging the map.',
+        title: 'Click and drag to rotate the camera.'
+      },
+      zoomCotrol: {
+        zoomInTip: 'Zoom in',
+        zoomResetTip: 'Reset zoom',
+        zoomOutTip: 'Zoom out'
+      },
+      print: {
+        printTip: 'Viewer screenshot/print',
+        printViewTitle: 'VueCesium Print View',
+        credit: 'Map Credits',
+        screenshot: 'Screenshot'
+      },
+
+      myLocation: {
+        myLocationTip: 'Centre map at your current location',
+        positioning: 'Positioning...',
+        fail: 'Positioning failed',
+        centreMap: 'My Location',
+        lat: 'Lat',
+        lng: 'Lng',
+        address: 'Address'
+      },
+      locationbar: {
+        lat: 'Lat',
+        lng: 'Lng',
+        zone: 'ZONE',
+        e: 'E',
+        n: 'N',
+        elev: 'Elev',
+        level: 'Level',
+        heading: 'H',
+        pitch: 'P',
+        roll: 'R',
+        cameraHeight: 'CameraH',
+        tip: 'Click to switch the mouse display coordinates to UTM projection coordinates'
+      },
+    },
+    overviewmap: {
+      show: 'Show',
+      hidden: 'Hidden'
+    }
+  }
+}
