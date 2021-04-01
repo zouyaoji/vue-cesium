feda95045a0d7dd1116ce208ddc8d3f9b30ed45a
zouyaoji <370681295@qq.com>
2021-04-01 12:40:36 +0800

    fix(composables): fix transformProp scope problem

--- packages/composables/use-common/index.ts
+++ packages/composables/use-common/index.ts
@@ -267,7 +267,7 @@ export default function(props, { emit }, vcInstance: VcComponentInternalInstance
       const cmpName = vcInstance.proxy.$options.name
       const propOption = cesiumProps[prop] && cesiumProps[prop][prop]
       return propOption && propOption.watcherOptions && !isEmptyObj(value)
-        ? propOption.watcherOptions.cesiumObjectBuilder.call(this, value)
+        ? propOption.watcherOptions.cesiumObjectBuilder.call(vcInstance, value)
         : isFunction(value) && cmpName && (cmpName.indexOf('graphics') !== -1 || cmpName === 'vc-entity')
           ? new Cesium.CallbackProperty(value, false)
           : value
