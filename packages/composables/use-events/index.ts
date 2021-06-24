7d5f405eea31c83e9b17badb5b37a5ef7e44326b
zouyaoji <370681295@qq.com>
2021-06-24 14:46:56 +0800

    fix: datasource component mouse event problem

--- packages/composables/use-events/index.ts
+++ packages/composables/use-events/index.ts
@@ -98,14 +98,14 @@ export default function (props, vcInstance: VcComponentInternalInstance, logger)
 
       this.pickedFeature = undefined
     } else {
-      if (this.pickedFeature && this.pickedFeature !== pickedFeature) {
+      if (this.pickedFeature && this.pickedFeature.id !== pickedFeature.id) {
         pickedFeatureAndCallbackNames.push({
           // 拾取到对象，this.pickedFeature也有记录，两者不同，说明操作到另外一个对象上去了
           callbackName: callbackNameOut,
           pickedFeature: this.pickedFeature,
         })
       }
-      if (callbackName === 'mousemove' && (!this.pickedFeature || this.pickedFeature !== pickedFeature)) {
+      if (callbackName === 'mousemove' && (!this.pickedFeature || this.pickedFeature.id !== pickedFeature.id)) {
         pickedFeatureAndCallbackNames.push({
           callbackName: 'mouseover',
           pickedFeature,
