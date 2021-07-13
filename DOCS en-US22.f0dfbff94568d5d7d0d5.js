d1c0f9c1b712109c95690369753469249d8ca135
zouyaoji <370681295@qq.com>
2021-07-13 01:09:39 +0000

    website deploy

---
+++ DOCS en-US22.f0dfbff94568d5d7d0d5.js
@@ -0,0 +1,264 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[15],{
+
+/***/ 549:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./node_modules/vue/dist/vue.esm-browser.js
+var vue_esm_browser = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/dist/templateLoader.js??ref--6!./node_modules/vue-loader/dist??ref--2-0!./website/md-loader!./website/docs/en-US/geometries/vc-geometry-polyline-simple.md?vue&type=template&id=5bf613bc
+
+var vc_geometry_polyline_simplevue_type_template_id_5bf613bc_hoisted_1 = {
+  class: "content element-doc"
+};
+
+var vc_geometry_polyline_simplevue_type_template_id_5bf613bc_hoisted_2 = /*#__PURE__*/Object(vue_esm_browser["l" /* createStaticVNode */])("<h2 id=\"vcgeometrypolylinesimple\"><a class=\"header-anchor\" href=\"#vcgeometrypolylinesimple\">¶</a> VcGeometryPolylineSimple</h2><p>Loading a polyline geometry modeled as a line strip. It is equivalent to initializing a <code>Cesium.SimplePolylineGeometry</code> instance.</p><p><strong>Note</strong>: It needs to be a subcomponent of <code>vc-instance-geometry</code> to load normally.</p><h3 id=\"basic-usage\"><a class=\"header-anchor\" href=\"#basic-usage\">¶</a> Basic usage</h3><p>Basic usage of VcGeometryPolylineSimple component.</p>", 5);
+
+var _hoisted_7 = /*#__PURE__*/Object(vue_esm_browser["n" /* createVNode */])("div", null, [/*#__PURE__*/Object(vue_esm_browser["n" /* createVNode */])("p", null, [/*#__PURE__*/Object(vue_esm_browser["m" /* createTextVNode */])("Use the "), /*#__PURE__*/Object(vue_esm_browser["n" /* createVNode */])("code", null, "vc-geometry-polyline-simple"), /*#__PURE__*/Object(vue_esm_browser["m" /* createTextVNode */])(" tag to add line strips on the viewer.")])], -1);
+
+var _hoisted_8 = /*#__PURE__*/Object(vue_esm_browser["n" /* createVNode */])("pre", null, [/*#__PURE__*/Object(vue_esm_browser["n" /* createVNode */])("code", {
+  class: "html"
+}, "<el-row ref=\"viewerContainer\" class=\"demo-viewer\">\n  <vc-viewer @ready=\"onViewerReady\">\n    <vc-primitive :appearance=\"appearance\" @click=\"onClicked\">\n      <vc-instance-geometry :attributes=\"attributes\">\n        <vc-geometry-polyline-simple\n          ref=\"geometryRef\"\n          :positions=\"[\n            { lng: 102.1, lat: 29.5 },\n            { lng: 106.2, lat: 29.5 },\n            { lng: 106.2, lat: 33.5 },\n            { lng: 108.2, lat: 35.5 },\n            { lng: 102.1, lat: 33.5 }\n          ]\"\n        ></vc-geometry-polyline-simple>\n      </vc-instance-geometry>\n    </vc-primitive>\n    <vc-layer-imagery>\n      <vc-provider-imagery-arcgis></vc-provider-imagery-arcgis>\n    </vc-layer-imagery>\n  </vc-viewer>\n  <el-row class=\"demo-toolbar\">\n    <el-button type=\"danger\" round @click=\"unload\">Unload</el-button>\n    <el-button type=\"danger\" round @click=\"load\">Load</el-button>\n    <el-button type=\"danger\" round @click=\"reload\">Reload</el-button>\n  </el-row>\n</el-row>\n\n<script>\n  import { ref, reactive, getCurrentInstance, onMounted, watch } from 'vue'\n  export default {\n    setup() {\n      // state\n      const instance = getCurrentInstance()\n      const geometryRef = ref(null)\n      const appearance = ref(null)\n      const attributes = ref(null)\n      // methods\n      const onClicked = e => {\n        console.log(e)\n      }\n      const unload = () => {\n        geometryRef.value.unload()\n      }\n      const reload = () => {\n        geometryRef.value.reload()\n      }\n      const load = () => {\n        geometryRef.value.load()\n      }\n      const onViewerReady = ({ Cesium, viewer }) => {\n        console.log('onViewerReady')\n        appearance.value = new Cesium.PerInstanceColorAppearance({\n          flat: true\n        })\n        attributes.value = {\n          color: new Cesium.ColorGeometryInstanceAttribute(Math.random(), Math.random(), Math.random())\n        }\n      }\n      // lifecycle\n      onMounted(() => {\n        geometryRef.value.createPromise.then(({ Cesium, viewer, cesiumObject }) => {\n          const boundingSphere = Cesium.BoundingSphere.fromPoints(cesiumObject._positions)\n          viewer.scene.camera.flyToBoundingSphere(boundingSphere)\n          console.log('All geometries are loaded.')\n        })\n      })\n      return {\n        unload,\n        reload,\n        load,\n        onClicked,\n        onViewerReady,\n        geometryRef,\n        appearance,\n        attributes\n      }\n    }\n  }\n</script>\n")], -1);
+
+var _hoisted_9 = /*#__PURE__*/Object(vue_esm_browser["l" /* createStaticVNode */])("<h3 id=\"props\"><a class=\"header-anchor\" href=\"#props\">¶</a> Props</h3><table><thead><tr><th>Name</th><th>Type</th><th>Default</th><th>Description</th><th>Accepted Values</th></tr></thead><tbody><tr><td>positions</td><td>Array</td><td></td><td><code>required</code> An array of Cartesian3 defining the positions in the polyline as a line strip.</td><td></td></tr><tr><td>colors</td><td>Array</td><td></td><td><code>optional</code> An Array of Color defining the per vertex or per segment colors.</td><td></td></tr><tr><td>colorsPerVertex</td><td>Boolean</td><td><code>false</code></td><td><code>optional</code> A boolean that determines whether the colors will be flat across each segment of the line or interpolated across the vertices.</td><td></td></tr><tr><td>arcType</td><td>Number</td><td><code>1</code></td><td><code>optional</code> The type of line the polyline segments must follow. <strong>NONE: 0, GEODESIC: 1, RHUMB: 2</strong></td><td>0/1/2</td></tr><tr><td>granularity</td><td>Number</td><td></td><td><code>optional</code> The distance, in radians, between each latitude and longitude if options.arcType is not ArcType.NONE. Determines the number of positions in the buffer.</td><td></td></tr><tr><td>ellipsoid</td><td>Object</td><td></td><td><code>optional</code> The ellipsoid to be used as a reference.</td><td></td></tr></tbody></table><h3 id=\"events\"><a class=\"header-anchor\" href=\"#events\">¶</a> Events</h3><table><thead><tr><th>Name</th><th>Parameters</th><th>Description</th></tr></thead><tbody><tr><td>beforeLoad</td><td>Vue Instance</td><td>Triggers before the cesiumObject is loaded.</td></tr><tr><td>ready</td><td>{Cesium, viewer, cesiumObject, vm}</td><td>Triggers when the cesiumObject is successfully loaded.</td></tr><tr><td>destroyed</td><td>Vue Instance</td><td>Triggers when the cesiumObject is destroyed.</td></tr></tbody></table><h3 id=\"reference\"><a class=\"header-anchor\" href=\"#reference\">¶</a> Reference</h3><ul><li>Refer to the official documentation: <strong><a href=\"https://cesium.com/docs/cesiumjs-ref-doc/SimplePolylineGeometry.html\">SimplePolylineGeometry</a></strong></li></ul>", 6);
+
+function vc_geometry_polyline_simplevue_type_template_id_5bf613bc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  var _component_vue_cesium_demo0 = Object(vue_esm_browser["O" /* resolveComponent */])("vue-cesium-demo0");
+
+  var _component_demo_block = Object(vue_esm_browser["O" /* resolveComponent */])("demo-block");
+
+  var _component_right_nav = Object(vue_esm_browser["O" /* resolveComponent */])("right-nav");
+
+  return Object(vue_esm_browser["F" /* openBlock */])(), Object(vue_esm_browser["i" /* createBlock */])("section", vc_geometry_polyline_simplevue_type_template_id_5bf613bc_hoisted_1, [vc_geometry_polyline_simplevue_type_template_id_5bf613bc_hoisted_2, Object(vue_esm_browser["n" /* createVNode */])(_component_demo_block, null, {
+    source: Object(vue_esm_browser["db" /* withCtx */])(function () {
+      return [Object(vue_esm_browser["n" /* createVNode */])(_component_vue_cesium_demo0)];
+    }),
+    highlight: Object(vue_esm_browser["db" /* withCtx */])(function () {
+      return [_hoisted_8];
+    }),
+    default: Object(vue_esm_browser["db" /* withCtx */])(function () {
+      return [_hoisted_7];
+    }),
+    _: 1
+  }), _hoisted_9, Object(vue_esm_browser["n" /* createVNode */])(_component_right_nav)]);
+}
+// CONCATENATED MODULE: ./website/docs/en-US/geometries/vc-geometry-polyline-simple.md?vue&type=template&id=5bf613bc
+
+// EXTERNAL MODULE: ./node_modules/@babel/runtime/helpers/extends.js
+var helpers_extends = __webpack_require__(2);
+var extends_default = /*#__PURE__*/__webpack_require__.n(helpers_extends);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib!./node_modules/vue-loader/dist??ref--2-0!./website/md-loader!./website/docs/en-US/geometries/vc-geometry-polyline-simple.md?vue&type=script&lang=ts
+
+
+/* harmony default export */ var vc_geometry_polyline_simplevue_type_script_lang_ts = ({
+  name: 'component-doc',
+  components: {
+    "vue-cesium-demo0": function () {
+      var _resolveComponent = vue_esm_browser["O" /* resolveComponent */],
+          _createVNode = vue_esm_browser["n" /* createVNode */],
+          _withCtx = vue_esm_browser["db" /* withCtx */],
+          _createTextVNode = vue_esm_browser["m" /* createTextVNode */],
+          _openBlock = vue_esm_browser["F" /* openBlock */],
+          _createBlock = vue_esm_browser["i" /* createBlock */];
+
+      var _hoisted_1 = /*#__PURE__*/_createTextVNode("Unload");
+
+      var _hoisted_2 = /*#__PURE__*/_createTextVNode("Load");
+
+      var _hoisted_3 = /*#__PURE__*/_createTextVNode("Reload");
+
+      function render(_ctx, _cache) {
+        var _component_vc_geometry_polyline_simple = _resolveComponent("vc-geometry-polyline-simple");
+
+        var _component_vc_instance_geometry = _resolveComponent("vc-instance-geometry");
+
+        var _component_vc_primitive = _resolveComponent("vc-primitive");
+
+        var _component_vc_provider_imagery_arcgis = _resolveComponent("vc-provider-imagery-arcgis");
+
+        var _component_vc_layer_imagery = _resolveComponent("vc-layer-imagery");
+
+        var _component_vc_viewer = _resolveComponent("vc-viewer");
+
+        var _component_el_button = _resolveComponent("el-button");
+
+        var _component_el_row = _resolveComponent("el-row");
+
+        return _openBlock(), _createBlock("div", null, [_createVNode(_component_el_row, {
+          ref: "viewerContainer",
+          class: "demo-viewer"
+        }, {
+          default: _withCtx(function () {
+            return [_createVNode(_component_vc_viewer, {
+              onReady: _ctx.onViewerReady
+            }, {
+              default: _withCtx(function () {
+                return [_createVNode(_component_vc_primitive, {
+                  appearance: _ctx.appearance,
+                  onClick: _ctx.onClicked
+                }, {
+                  default: _withCtx(function () {
+                    return [_createVNode(_component_vc_instance_geometry, {
+                      attributes: _ctx.attributes
+                    }, {
+                      default: _withCtx(function () {
+                        return [_createVNode(_component_vc_geometry_polyline_simple, {
+                          ref: "geometryRef",
+                          positions: [{
+                            lng: 102.1,
+                            lat: 29.5
+                          }, {
+                            lng: 106.2,
+                            lat: 29.5
+                          }, {
+                            lng: 106.2,
+                            lat: 33.5
+                          }, {
+                            lng: 108.2,
+                            lat: 35.5
+                          }, {
+                            lng: 102.1,
+                            lat: 33.5
+                          }]
+                        }, null, 8, ["positions"])];
+                      }),
+                      _: 1
+                    }, 8, ["attributes"])];
+                  }),
+                  _: 1
+                }, 8, ["appearance", "onClick"]), _createVNode(_component_vc_layer_imagery, null, {
+                  default: _withCtx(function () {
+                    return [_createVNode(_component_vc_provider_imagery_arcgis)];
+                  }),
+                  _: 1
+                })];
+              }),
+              _: 1
+            }, 8, ["onReady"]), _createVNode(_component_el_row, {
+              class: "demo-toolbar"
+            }, {
+              default: _withCtx(function () {
+                return [_createVNode(_component_el_button, {
+                  type: "danger",
+                  round: "",
+                  onClick: _ctx.unload
+                }, {
+                  default: _withCtx(function () {
+                    return [_hoisted_1];
+                  }),
+                  _: 1
+                }, 8, ["onClick"]), _createVNode(_component_el_button, {
+                  type: "danger",
+                  round: "",
+                  onClick: _ctx.load
+                }, {
+                  default: _withCtx(function () {
+                    return [_hoisted_2];
+                  }),
+                  _: 1
+                }, 8, ["onClick"]), _createVNode(_component_el_button, {
+                  type: "danger",
+                  round: "",
+                  onClick: _ctx.reload
+                }, {
+                  default: _withCtx(function () {
+                    return [_hoisted_3];
+                  }),
+                  _: 1
+                }, 8, ["onClick"])];
+              }),
+              _: 1
+            })];
+          }),
+          _: 1
+        }, 512)]);
+      }
+
+      var ref = vue_esm_browser["K" /* ref */],
+          reactive = vue_esm_browser["J" /* reactive */],
+          getCurrentInstance = vue_esm_browser["q" /* getCurrentInstance */],
+          onMounted = vue_esm_browser["C" /* onMounted */],
+          watch = vue_esm_browser["bb" /* watch */];
+      var democomponentExport = {
+        setup: function setup() {
+          // state
+          var instance = getCurrentInstance();
+          var geometryRef = ref(null);
+          var appearance = ref(null);
+          var attributes = ref(null); // methods
+
+          var onClicked = function onClicked(e) {
+            console.log(e);
+          };
+
+          var unload = function unload() {
+            geometryRef.value.unload();
+          };
+
+          var reload = function reload() {
+            geometryRef.value.reload();
+          };
+
+          var load = function load() {
+            geometryRef.value.load();
+          };
+
+          var onViewerReady = function onViewerReady(_ref) {
+            var Cesium = _ref.Cesium,
+                viewer = _ref.viewer;
+            console.log('onViewerReady');
+            appearance.value = new Cesium.PerInstanceColorAppearance({
+              flat: true
+            });
+            attributes.value = {
+              color: new Cesium.ColorGeometryInstanceAttribute(Math.random(), Math.random(), Math.random())
+            };
+          }; // lifecycle
+
+
+          onMounted(function () {
+            geometryRef.value.createPromise.then(function (_ref2) {
+              var Cesium = _ref2.Cesium,
+                  viewer = _ref2.viewer,
+                  cesiumObject = _ref2.cesiumObject;
+              var boundingSphere = Cesium.BoundingSphere.fromPoints(cesiumObject._positions);
+              viewer.scene.camera.flyToBoundingSphere(boundingSphere);
+              console.log('All geometries are loaded.');
+            });
+          });
+          return {
+            unload: unload,
+            reload: reload,
+            load: load,
+            onClicked: onClicked,
+            onViewerReady: onViewerReady,
+            geometryRef: geometryRef,
+            appearance: appearance,
+            attributes: attributes
+          };
+        }
+      };
+      return extends_default()({
+        render: render
+      }, democomponentExport);
+    }()
+  }
+});
+// CONCATENATED MODULE: ./website/docs/en-US/geometries/vc-geometry-polyline-simple.md?vue&type=script&lang=ts
+ 
+// CONCATENATED MODULE: ./website/docs/en-US/geometries/vc-geometry-polyline-simple.md
+
+
+
+vc_geometry_polyline_simplevue_type_script_lang_ts.render = vc_geometry_polyline_simplevue_type_template_id_5bf613bc_render
+
+/* harmony default export */ var vc_geometry_polyline_simple = __webpack_exports__["default"] = (vc_geometry_polyline_simplevue_type_script_lang_ts);
+
+/***/ })
+
+}]);
