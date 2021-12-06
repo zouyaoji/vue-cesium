5dee095f61794eaabdfe03dfd16c6fbb979b3272
zouyaoji <370681295@qq.com>
2021-12-06 09:46:28 +0000

    website deploy

---
+++ DOCS zh-CN56.ab70d148b1fb13c66622.js
@@ -0,0 +1,301 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[148],{
+
+/***/ 596:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./node_modules/.pnpm/vue@3.2.23/node_modules/vue/dist/vue.esm-browser.js
+var vue_esm_browser = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./node_modules/.pnpm/babel-loader@8.2.3_bcd6ea64369259a9a094ff5ba7188bb6/node_modules/babel-loader/lib!./node_modules/.pnpm/vue-loader@16.5.0_b706f451d6404ce468e2858b35d8eb7f/node_modules/vue-loader/dist/templateLoader.js??ref--6!./node_modules/.pnpm/vue-loader@16.5.0_b706f451d6404ce468e2858b35d8eb7f/node_modules/vue-loader/dist??ref--2-0!./website/md-loader!./website/docs/zh-CN/platforms/vc-demo-supermap.md?vue&type=template&id=0e4060be
+
+const vc_demo_supermapvue_type_template_id_0e4060be_hoisted_1 = {
+  class: "content vue-cesium-doc"
+};
+
+const vc_demo_supermapvue_type_template_id_0e4060be_hoisted_2 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("SuperMap Demo ");
+
+const vc_demo_supermapvue_type_template_id_0e4060be_hoisted_3 = /*#__PURE__*/Object(vue_esm_browser["createStaticVNode"])("<p>vue-cesium 使用 SuperMap iClient3D for WebGL 开发时只需要在引入 VueCesium 时通过配置项 <code>cesiumPath</code> 指定为 iClient3D 库地址即可。</p><pre class=\"example-code\"><code class=\"hljs language-javascript\"><span class=\"hljs-keyword\">import</span> { createApp } <span class=\"hljs-keyword\">from</span> <span class=\"hljs-string\">&#39;vue&#39;</span>\n<span class=\"hljs-keyword\">import</span> <span class=\"hljs-title class_\">VueCesium</span> <span class=\"hljs-keyword\">from</span> <span class=\"hljs-string\">&#39;vue-cesium&#39;</span>\n<span class=\"hljs-keyword\">import</span> <span class=\"hljs-string\">&#39;vue-cesium/dist/index.css&#39;</span>\n<span class=\"hljs-keyword\">import</span> <span class=\"hljs-title class_\">App</span> <span class=\"hljs-keyword\">from</span> <span class=\"hljs-string\">&#39;./App.vue&#39;</span>\n\n<span class=\"hljs-keyword\">const</span> app = <span class=\"hljs-title function_\">createApp</span>(<span class=\"hljs-title class_\">App</span>)\napp.<span class=\"hljs-title function_\">use</span>(<span class=\"hljs-title class_\">VueCesium</span>, {\n  <span class=\"hljs-attr\">cesiumPath</span>: <span class=\"hljs-string\">&#39;https://www.supermapol.com/earth/Build/Cesium/Cesium.js&#39;</span>\n})\napp.<span class=\"hljs-title function_\">mount</span>(<span class=\"hljs-string\">&#39;#app&#39;</span>)\n</code><span class=\"lang-mark\">js</span></pre><p>或者在 <code>vc-viewer</code> 组件上将 <code>cesiumPath</code> 地址指定为 iClient3D 路径。 如下面的例子：</p>", 3);
+
+const _hoisted_6 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("基础用法 ");
+
+const _hoisted_7 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("p", null, "使用 VueCesium 加载 SuperMap iClient3D for WebGL", -1);
+
+const _hoisted_8 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("div", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("p", null, [/*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("通过 "), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("code", null, "vc-viewer"), /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 的 cesiumPath 属性指定使用 SuperMap iClient3D for WebGL。")])], -1);
+
+const _hoisted_9 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("pre", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("code", {
+  class: "html"
+}, "<el-row ref=\"viewerContainer\" class=\"demo-viewer\">\n  <vc-viewer\n    ref=\"vcViewer\"\n    :cesiumPath=\"cesiumPath\"\n    :animation=\"animation\"\n    :timeline=\"timeline\"\n    :fullscreenButton=\"fullscreenButton\"\n    :fullscreenElement=\"fullscreenElement\"\n    @ready=\"onViewerReady\"\n    @leftClick=\"onLeftClick\"\n  >\n    <vc-navigation :offset=\"offset\" @compass-evt=\"onNavigationEvt\" :otherOpts=\"otherOpts\" @zoom-evt=\"onNavigationEvt\"></vc-navigation>\n    <!-- <vc-entity v-model:billboard=\"billboard\" ref=\"entity\" @click=\"onEntityClick\" :position=\"{lng: 108, lat: 32}\" :point=\"point\" :label=\"label\">\n      <vc-graphics-billboard ref=\"billboard\" image=\"https://zouyaoji.top/vue-cesium/favicon.png\"></vc-graphics-billboard>\n      <vc-graphics-rectangle :coordinates=\"[130, 20, 80, 25]\" material=\"green\"></vc-graphics-rectangle>\n    </vc-entity> -->\n    <!-- 天地图注记 -->\n    <vc-layer-imagery :sortOrder=\"20\">\n      <vc-provider-imagery-tianditu mapStyle=\"cva_c\" token=\"436ce7e50d27eede2f2929307e6b33c0\"></vc-provider-imagery-tianditu>\n    </vc-layer-imagery>\n    <!-- 天地图影像 -->\n    <vc-layer-imagery :sortOrder=\"10\">\n      <vc-provider-imagery-tianditu mapStyle=\"img_c\" token=\"436ce7e50d27eede2f2929307e6b33c0\"></vc-provider-imagery-tianditu>\n    </vc-layer-imagery>\n  </vc-viewer>\n  <el-row class=\"demo-toolbar\">\n    <el-row>\n      <el-button type=\"danger\" round @click=\"unload\">销毁</el-button>\n      <el-button type=\"danger\" round @click=\"load\">加载</el-button>\n      <el-button type=\"danger\" round @click=\"reload\">重载</el-button>\n    </el-row>\n  </el-row>\n</el-row>\n\n<script>\n  export default {\n    data() {\n      return {\n        animation: true,\n        timeline: true,\n        fullscreenButton: true,\n        fullscreenElement: document.body,\n        point: {\n          pixelSize: 28,\n          color: 'red'\n        },\n        label: {\n          text: 'Hello World',\n          pixelOffset: [0, 150]\n        },\n        billboard: {},\n        offset: [10, 25],\n        otherOpts: {\n          offset: [0, 32],\n          position: 'bottom-right'\n        },\n        cesiumPath: 'https://www.supermapol.com/earth/Build/Cesium/Cesium.js'\n      }\n    },\n    mounted() {\n      this.$refs.vcViewer.createPromise.then(({ Cesium, viewer }) => {\n        console.log('viewer is loaded.')\n      })\n    },\n    methods: {\n      onViewerReady({ Cesium, viewer }) {\n        viewer.imageryLayers.removeAll()\n        console.log(viewer)\n      },\n      onNavigationEvt(e) {\n        console.log(e)\n      },\n      onEntityClick(e) {\n        console.log(e)\n      },\n      onLeftClick(e) {\n        console.log(e)\n      },\n      load() {\n        this.$refs.vcViewer.load().then(e => {\n          console.log(e)\n        })\n      },\n      unload() {\n        this.$refs.vcViewer.unload().then(e => {\n          console.log(e)\n        })\n      },\n      reload() {\n        this.$refs.vcViewer.reload().then(e => {\n          console.log(e)\n        })\n      }\n    }\n  }\n</script>\n")], -1);
+
+const _hoisted_10 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("参考 ");
+
+const _hoisted_11 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("官网：");
+
+const _hoisted_12 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" SuperMap iClient3D for WebGL");
+
+function vc_demo_supermapvue_type_template_id_0e4060be_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_app_link = Object(vue_esm_browser["resolveComponent"])("app-link");
+
+  const _component_app_heading = Object(vue_esm_browser["resolveComponent"])("app-heading");
+
+  const _component_vue_cesium_demo0 = Object(vue_esm_browser["resolveComponent"])("vue-cesium-demo0");
+
+  const _component_demo_block = Object(vue_esm_browser["resolveComponent"])("demo-block");
+
+  const _component_right_nav = Object(vue_esm_browser["resolveComponent"])("right-nav");
+
+  return Object(vue_esm_browser["openBlock"])(), Object(vue_esm_browser["createElementBlock"])("section", vc_demo_supermapvue_type_template_id_0e4060be_hoisted_1, [Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "supermap-demo",
+    tabindex: "-1",
+    content: "SuperMap Demo",
+    href: "#supermap-demo",
+    level: "2"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [vc_demo_supermapvue_type_template_id_0e4060be_hoisted_2, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#supermap-demo"
+    })]),
+    _: 1
+  }), vc_demo_supermapvue_type_template_id_0e4060be_hoisted_3, Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "ji-chu-yong-fa",
+    tabindex: "-1",
+    content: "基础用法",
+    href: "#ji-chu-yong-fa",
+    level: "3"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_6, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#ji-chu-yong-fa"
+    })]),
+    _: 1
+  }), _hoisted_7, Object(vue_esm_browser["createVNode"])(_component_demo_block, null, {
+    source: Object(vue_esm_browser["withCtx"])(() => [Object(vue_esm_browser["createVNode"])(_component_vue_cesium_demo0)]),
+    highlight: Object(vue_esm_browser["withCtx"])(() => [_hoisted_9]),
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_8]),
+    _: 1
+  }), Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "can-kao",
+    tabindex: "-1",
+    content: "参考",
+    href: "#can-kao",
+    level: "3"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_10, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#can-kao"
+    })]),
+    _: 1
+  }), Object(vue_esm_browser["createElementVNode"])("ul", null, [Object(vue_esm_browser["createElementVNode"])("li", null, [_hoisted_11, Object(vue_esm_browser["createElementVNode"])("strong", null, [Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "http://support.supermap.com.cn:8090/webgl/web/index.html"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_12]),
+    _: 1
+  })])])]), Object(vue_esm_browser["createVNode"])(_component_right_nav)]);
+}
+// CONCATENATED MODULE: ./website/docs/zh-CN/platforms/vc-demo-supermap.md?vue&type=template&id=0e4060be
+
+// CONCATENATED MODULE: ./node_modules/.pnpm/babel-loader@8.2.3_bcd6ea64369259a9a094ff5ba7188bb6/node_modules/babel-loader/lib!./node_modules/.pnpm/vue-loader@16.5.0_b706f451d6404ce468e2858b35d8eb7f/node_modules/vue-loader/dist??ref--2-0!./website/md-loader!./website/docs/zh-CN/platforms/vc-demo-supermap.md?vue&type=script&lang=ts
+
+/* harmony default export */ var vc_demo_supermapvue_type_script_lang_ts = ({
+  name: 'component-doc',
+  components: {
+    "vue-cesium-demo0": function () {
+      const {
+        resolveComponent: _resolveComponent,
+        createVNode: _createVNode,
+        withCtx: _withCtx,
+        createTextVNode: _createTextVNode,
+        openBlock: _openBlock,
+        createElementBlock: _createElementBlock
+      } = vue_esm_browser;
+
+      const _hoisted_1 = /*#__PURE__*/_createTextVNode("销毁");
+
+      const _hoisted_2 = /*#__PURE__*/_createTextVNode("加载");
+
+      const _hoisted_3 = /*#__PURE__*/_createTextVNode("重载");
+
+      function render(_ctx, _cache) {
+        const _component_vc_navigation = _resolveComponent("vc-navigation");
+
+        const _component_vc_provider_imagery_tianditu = _resolveComponent("vc-provider-imagery-tianditu");
+
+        const _component_vc_layer_imagery = _resolveComponent("vc-layer-imagery");
+
+        const _component_vc_viewer = _resolveComponent("vc-viewer");
+
+        const _component_el_button = _resolveComponent("el-button");
+
+        const _component_el_row = _resolveComponent("el-row");
+
+        return _openBlock(), _createElementBlock("div", null, [_createVNode(_component_el_row, {
+          ref: "viewerContainer",
+          class: "demo-viewer"
+        }, {
+          default: _withCtx(() => [_createVNode(_component_vc_viewer, {
+            ref: "vcViewer",
+            cesiumPath: _ctx.cesiumPath,
+            animation: _ctx.animation,
+            timeline: _ctx.timeline,
+            fullscreenButton: _ctx.fullscreenButton,
+            fullscreenElement: _ctx.fullscreenElement,
+            onReady: _ctx.onViewerReady,
+            onLeftClick: _ctx.onLeftClick
+          }, {
+            default: _withCtx(() => [_createVNode(_component_vc_navigation, {
+              offset: _ctx.offset,
+              onCompassEvt: _ctx.onNavigationEvt,
+              otherOpts: _ctx.otherOpts,
+              onZoomEvt: _ctx.onNavigationEvt
+            }, null, 8, ["offset", "onCompassEvt", "otherOpts", "onZoomEvt"]), _createVNode(_component_vc_layer_imagery, {
+              sortOrder: 20
+            }, {
+              default: _withCtx(() => [_createVNode(_component_vc_provider_imagery_tianditu, {
+                mapStyle: "cva_c",
+                token: "436ce7e50d27eede2f2929307e6b33c0"
+              })]),
+              _: 1
+            }), _createVNode(_component_vc_layer_imagery, {
+              sortOrder: 10
+            }, {
+              default: _withCtx(() => [_createVNode(_component_vc_provider_imagery_tianditu, {
+                mapStyle: "img_c",
+                token: "436ce7e50d27eede2f2929307e6b33c0"
+              })]),
+              _: 1
+            })]),
+            _: 1
+          }, 8, ["cesiumPath", "animation", "timeline", "fullscreenButton", "fullscreenElement", "onReady", "onLeftClick"]), _createVNode(_component_el_row, {
+            class: "demo-toolbar"
+          }, {
+            default: _withCtx(() => [_createVNode(_component_el_row, null, {
+              default: _withCtx(() => [_createVNode(_component_el_button, {
+                type: "danger",
+                round: "",
+                onClick: _ctx.unload
+              }, {
+                default: _withCtx(() => [_hoisted_1]),
+                _: 1
+              }, 8, ["onClick"]), _createVNode(_component_el_button, {
+                type: "danger",
+                round: "",
+                onClick: _ctx.load
+              }, {
+                default: _withCtx(() => [_hoisted_2]),
+                _: 1
+              }, 8, ["onClick"]), _createVNode(_component_el_button, {
+                type: "danger",
+                round: "",
+                onClick: _ctx.reload
+              }, {
+                default: _withCtx(() => [_hoisted_3]),
+                _: 1
+              }, 8, ["onClick"])]),
+              _: 1
+            })]),
+            _: 1
+          })]),
+          _: 1
+        }, 512)]);
+      }
+
+      const democomponentExport = {
+        data() {
+          return {
+            animation: true,
+            timeline: true,
+            fullscreenButton: true,
+            fullscreenElement: document.body,
+            point: {
+              pixelSize: 28,
+              color: 'red'
+            },
+            label: {
+              text: 'Hello World',
+              pixelOffset: [0, 150]
+            },
+            billboard: {},
+            offset: [10, 25],
+            otherOpts: {
+              offset: [0, 32],
+              position: 'bottom-right'
+            },
+            cesiumPath: 'https://www.supermapol.com/earth/Build/Cesium/Cesium.js'
+          };
+        },
+
+        mounted() {
+          this.$refs.vcViewer.createPromise.then(_ref => {
+            let {
+              Cesium,
+              viewer
+            } = _ref;
+            console.log('viewer is loaded.');
+          });
+        },
+
+        methods: {
+          onViewerReady(_ref2) {
+            let {
+              Cesium,
+              viewer
+            } = _ref2;
+            viewer.imageryLayers.removeAll();
+            console.log(viewer);
+          },
+
+          onNavigationEvt(e) {
+            console.log(e);
+          },
+
+          onEntityClick(e) {
+            console.log(e);
+          },
+
+          onLeftClick(e) {
+            console.log(e);
+          },
+
+          load() {
+            this.$refs.vcViewer.load().then(e => {
+              console.log(e);
+            });
+          },
+
+          unload() {
+            this.$refs.vcViewer.unload().then(e => {
+              console.log(e);
+            });
+          },
+
+          reload() {
+            this.$refs.vcViewer.reload().then(e => {
+              console.log(e);
+            });
+          }
+
+        }
+      };
+      return {
+        render,
+        ...democomponentExport
+      };
+    }()
+  }
+});
+// CONCATENATED MODULE: ./website/docs/zh-CN/platforms/vc-demo-supermap.md?vue&type=script&lang=ts
+ 
+// CONCATENATED MODULE: ./website/docs/zh-CN/platforms/vc-demo-supermap.md
+
+
+
+vc_demo_supermapvue_type_script_lang_ts.render = vc_demo_supermapvue_type_template_id_0e4060be_render
+
+/* harmony default export */ var vc_demo_supermap = __webpack_exports__["default"] = (vc_demo_supermapvue_type_script_lang_ts);
+
+/***/ })
+
+}]);
