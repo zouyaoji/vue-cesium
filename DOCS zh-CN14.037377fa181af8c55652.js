5ce9d43e3b6cc4c6b17163533fecd10734449adf
zouyaoji <370681295@qq.com>
2022-02-13 16:07:42 +0000

    website deploy

---
+++ DOCS zh-CN14.037377fa181af8c55652.js
@@ -0,0 +1,142 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[105],{
+
+/***/ 762:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./node_modules/.pnpm/vue@3.2.30/node_modules/vue/dist/vue.esm-browser.js
+var vue_esm_browser = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./node_modules/.pnpm/babel-loader@8.2.3_bcd6ea64369259a9a094ff5ba7188bb6/node_modules/babel-loader/lib!./node_modules/.pnpm/vue-loader@16.5.0_2a2707c7f6fb238ee6d53f3ae7e6e321/node_modules/vue-loader/dist/templateLoader.js??ref--6!./node_modules/.pnpm/vue-loader@16.5.0_2a2707c7f6fb238ee6d53f3ae7e6e321/node_modules/vue-loader/dist??ref--3-0!./website/md-loader!./website/docs/zh-CN/donations.md?vue&type=template&id=1e202f3e
+
+const _hoisted_1 = {
+  class: "content vue-cesium-doc"
+};
+
+const _hoisted_2 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("赞助 ");
+
+const _hoisted_3 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("赞助 vue-cesium ");
+
+const _hoisted_4 = /*#__PURE__*/Object(vue_esm_browser["createStaticVNode"])("<ul><li><p><code>vue-cesium</code> 采用 MIT 开源协议，承诺框架本身与官网文档永久免费开放， 但是框架的日常更新与社区运营需要付出大量的精力，靠爱发电难以长久，如果 <code>vue-cesium</code> 帮助到了您，您可以友情支持一下 <code>vue-cesium</code>。</p></li><li><p>微信 / 支付宝</p><div style=\"display:flex;\"><img alt=\"wechat\" title=\"wechat\" src=\"https://zouyaoji.top/vue-cesium/images/wechat.png\"><img alt=\"alipay\" title=\"alipay\" src=\"https://zouyaoji.top/vue-cesium/images/alipay.png\"></div></li><li><p>江山父老能容我，不使人间造孽钱。</p></li></ul>", 1);
+
+const _hoisted_5 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("捐赠列表 ");
+
+const _hoisted_6 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("p", null, "特别感谢以下捐赠者：", -1);
+
+const _hoisted_7 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("thead", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("tr", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("th", null, "金额"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("th", null, "来源"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("th", null, "赞助人"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("th", null, "留言"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("th", null, "时间")])], -1);
+
+const _hoisted_8 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("tr", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "¥ 66.66"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "QQ"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "Levi"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "恭喜发财"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "2022-01-28")], -1);
+
+const _hoisted_9 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("tr", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "¥ 66.66"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "QQ"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "Levi"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "多谢大佬开源"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "2022-01-11")], -1);
+
+const _hoisted_10 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("tr", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "¥ 88.0"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "WeChat"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "*丫"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "有学习群吗？"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "2021-10-28")], -1);
+
+const _hoisted_11 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "¥ 200.0", -1);
+
+const _hoisted_12 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "WeChat", -1);
+
+const _hoisted_13 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("木遥");
+
+const _hoisted_14 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "mars3d 谢谢", -1);
+
+const _hoisted_15 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "2021-07-05", -1);
+
+const _hoisted_16 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "¥ 150.0", -1);
+
+const _hoisted_17 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "WeChat", -1);
+
+const _hoisted_18 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("*飞");
+
+const _hoisted_19 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "持续迭代，更牛的模块", -1);
+
+const _hoisted_20 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "2021-06-09", -1);
+
+const _hoisted_21 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "$ 50.0", -1);
+
+const _hoisted_22 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "Paypal", -1);
+
+const _hoisted_23 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("boriskogan81");
+
+const _hoisted_24 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "Thank you for your great library!", -1);
+
+const _hoisted_25 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "2020-05-27", -1);
+
+const _hoisted_26 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("tr", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "¥ 100.0"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "AliPay"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "-。向日葵"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "咨询费"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", null, "2020-05-21")], -1);
+
+const _hoisted_27 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("p", null, "如果有漏掉的同学，请联系我；如果您不想自己的名字显示在这里，可以发邮件或者 qq 告诉我", -1);
+
+function render(_ctx, _cache) {
+  const _component_app_link = Object(vue_esm_browser["resolveComponent"])("app-link");
+
+  const _component_app_heading = Object(vue_esm_browser["resolveComponent"])("app-heading");
+
+  const _component_right_nav = Object(vue_esm_browser["resolveComponent"])("right-nav");
+
+  return Object(vue_esm_browser["openBlock"])(), Object(vue_esm_browser["createElementBlock"])("section", _hoisted_1, [Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "zan-zhu",
+    tabindex: "-1",
+    content: "赞助",
+    href: "#zan-zhu",
+    level: "2"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_2, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#zan-zhu"
+    })]),
+    _: 1
+  }), Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "zan-zhu-vue-cesium",
+    tabindex: "-1",
+    content: "赞助 vue-cesium",
+    href: "#zan-zhu-vue-cesium",
+    level: "3"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_3, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#zan-zhu-vue-cesium"
+    })]),
+    _: 1
+  }), _hoisted_4, Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "juan-zeng-lie-biao",
+    tabindex: "-1",
+    content: "捐赠列表",
+    href: "#juan-zeng-lie-biao",
+    level: "3"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_5, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#juan-zeng-lie-biao"
+    })]),
+    _: 1
+  }), Object(vue_esm_browser["createElementVNode"])("ul", null, [Object(vue_esm_browser["createElementVNode"])("li", null, [_hoisted_6, Object(vue_esm_browser["createElementVNode"])("table", null, [_hoisted_7, Object(vue_esm_browser["createElementVNode"])("tbody", null, [_hoisted_8, _hoisted_9, _hoisted_10, Object(vue_esm_browser["createElementVNode"])("tr", null, [_hoisted_11, _hoisted_12, Object(vue_esm_browser["createElementVNode"])("td", null, [Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://github.com/muyao1987"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_13]),
+    _: 1
+  })]), _hoisted_14, _hoisted_15]), Object(vue_esm_browser["createElementVNode"])("tr", null, [_hoisted_16, _hoisted_17, Object(vue_esm_browser["createElementVNode"])("td", null, [Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://github.com/ZephyrTan"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_18]),
+    _: 1
+  })]), _hoisted_19, _hoisted_20]), Object(vue_esm_browser["createElementVNode"])("tr", null, [_hoisted_21, _hoisted_22, Object(vue_esm_browser["createElementVNode"])("td", null, [Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://github.com/boriskogan81"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_23]),
+    _: 1
+  })]), _hoisted_24, _hoisted_25]), _hoisted_26])]), _hoisted_27])]), Object(vue_esm_browser["createVNode"])(_component_right_nav)]);
+}
+// CONCATENATED MODULE: ./website/docs/zh-CN/donations.md?vue&type=template&id=1e202f3e
+
+// CONCATENATED MODULE: ./website/docs/zh-CN/donations.md
+
+const script = {}
+script.render = render
+
+/* harmony default export */ var donations = __webpack_exports__["default"] = (script);
+
+/***/ })
+
+}]);
