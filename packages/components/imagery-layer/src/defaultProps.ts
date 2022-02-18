c27c26a7028a95646b5daf6fc4330b5fe73d9e6a
zouyaoji <370681295@qq.com>
2022-02-18 23:54:12 +0800

    refactor(imagery-layer): improve types

--- packages/components/imagery-layer/src/defaultProps.ts
+++ packages/components/imagery-layer/src/defaultProps.ts
@@ -1,46 +1,56 @@
-import { rectangle, colorToAlpha, cutoutRectangle } from '@vue-cesium/utils/cesium-props'
+/*
+ * @Author: zouyaoji@https://github.com/zouyaoji
+ * @Date: 2021-10-27 15:54:13
+ * @LastEditTime: 2022-02-15 17:45:45
+ * @LastEditors: zouyaoji
+ * @Description:
+ * @FilePath: \vue-cesium@next\packages\components\imagery-layer\src\defaultProps.ts
+ */
+import { rectangle, colorToAlpha, cutoutRectangle, show } from '@vue-cesium/utils/cesium-props'
+import type { AnyFunction, VcImageryProvider } from '@vue-cesium/utils/types'
+import type { PropType } from 'vue'
 export default {
-  imageryProvider: Object,
+  imageryProvider: Object as PropType<VcImageryProvider>,
   ...rectangle,
   alpha: {
-    type: Number,
+    type: [Number, Function] as PropType<number | AnyFunction<any>>,
     default: 1.0
   },
   nightAlpha: {
-    type: Number,
+    type: [Number, Function] as PropType<number | AnyFunction<any>>,
     default: 1.0
   },
   dayAlpha: {
-    type: Number,
+    type: [Number, Function] as PropType<number | AnyFunction<any>>,
     default: 1.0
   },
   brightness: {
-    type: Number,
+    type: [Number, Function] as PropType<number | AnyFunction<any>>,
     default: 1.0
   },
   contrast: {
-    type: Number,
+    type: [Number, Function] as PropType<number | AnyFunction<any>>,
     default: 1.0
   },
   hue: {
-    type: Number,
+    type: [Number, Function] as PropType<number | AnyFunction<any>>,
     default: 0.0
   },
   saturation: {
-    type: Number,
+    type: [Number, Function] as PropType<number | AnyFunction<any>>,
     default: 1.0
   },
   gamma: {
-    type: Number,
+    type: [Number, Function] as PropType<number | AnyFunction<any>>,
     default: 1.0
   },
-  splitDirection: Number,
+  splitDirection: {
+    type: [Number, Function] as PropType<number | AnyFunction<any>>,
+    default: 0
+  },
   minificationFilter: Number,
   magnificationFilter: Number,
-  show: {
-    type: Boolean,
-    default: true
-  },
+  ...show,
   maximumAnisotropy: Number,
   minimumTerrainLevel: Number,
   maximumTerrainLevel: Number,
