446638d75c281cf12dd5f4ea173ca9d3ecb883f2
zouyaoji <370681295@qq.com>
2022-03-08 18:05:48 +0800

    refactor: refactor useVueCesium method

--- packages/composables/use-vue-cesium/index.ts
+++ packages/composables/use-vue-cesium/index.ts
@@ -1,20 +1,21 @@
 /*
  * @Author: zouyaoji@https://github.com/zouyaoji
  * @Date: 2021-04-06 09:21:02
- * @LastEditTime: 2022-02-25 23:27:55
+ * @LastEditTime: 2022-03-04 00:59:58
  * @LastEditors: zouyaoji
  * @Description:
  * @FilePath: \vue-cesium@next\packages\composables\use-vue-cesium\index.ts
  */
-import { getCurrentInstance } from 'vue'
+import { getCurrentInstance, inject } from 'vue'
 import { VcViewerProvider } from '@vue-cesium/utils/types'
 import useLog from '@vue-cesium/composables/private/use-log'
+import { vcKey } from '@vue-cesium/utils/config'
 
 export default function useVueCesium(): VcViewerProvider {
   const instance = getCurrentInstance()
   const logger = useLog(undefined)
   if (instance) {
-    return instance.appContext.config.globalProperties.$VueCesium as VcViewerProvider
+    return inject<VcViewerProvider>(vcKey) || (instance.appContext.config.globalProperties.$VueCesium as VcViewerProvider)
   } else {
     logger.warn('VueCesium useVueCesium() can only be used inside setup().')
   }
