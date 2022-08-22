28aa240fbde9a07c0709e82eeb910a8c8c864a3a
zouyaoji <370681295@qq.com>
2022-08-22 13:38:38 +0000

    website deploy

---
+++ DOCS zh-CN92.8397624ef264c26471a0.js
@@ -0,0 +1,275 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[194],{
+
+/***/ 707:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./node_modules/.pnpm/vue@3.2.30/node_modules/vue/dist/vue.esm-browser.js
+var vue_esm_browser = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./node_modules/.pnpm/babel-loader@8.2.3_xtlouzbwsjm2tieu75n2ogelwy/node_modules/babel-loader/lib!./node_modules/.pnpm/vue-loader@16.5.0_fitqpr7w7mry5zwvh45opzxdee/node_modules/vue-loader/dist/templateLoader.js??ref--6!./node_modules/.pnpm/vue-loader@16.5.0_fitqpr7w7mry5zwvh45opzxdee/node_modules/vue-loader/dist??ref--3-0!./website/md-loader!./website/docs/zh-CN/providers/vc-terrain-provider-arcgis.md?vue&type=template&id=da1455e6
+
+const vc_terrain_provider_arcgisvue_type_template_id_da1455e6_hoisted_1 = {
+  class: "content vue-cesium-doc"
+};
+
+const vc_terrain_provider_arcgisvue_type_template_id_da1455e6_hoisted_2 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("VcTerrainProviderArcgis ");
+
+const vc_terrain_provider_arcgisvue_type_template_id_da1455e6_hoisted_3 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("p", null, [/*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("加载 ArcGISTiledElevation 格式地形，相当于初始化一个 "), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("code", null, "Cesium.ArcGISTiledElevationTerrainProvider"), /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 实例。")], -1);
+
+const vc_terrain_provider_arcgisvue_type_template_id_da1455e6_hoisted_4 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("基础用法 ");
+
+const _hoisted_5 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("p", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("code", null, "vc-terrain-provider-arcgis"), /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 组件的基础用法。")], -1);
+
+const _hoisted_6 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("div", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("p", null, [/*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("使用 "), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("code", null, "vc-terrain-provider-arcgis"), /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 标签在三维球上添加由 ArcGIS MapServer 提供的在线地形瓦片服务。")])], -1);
+
+const _hoisted_7 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("pre", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("code", {
+  class: "html"
+}, "<el-row ref=\"viewerContainer\" class=\"demo-viewer\">\n  <vc-viewer @ready=\"onViewerReady\" :imageryProvider=\"imageryProvider\">\n    <vc-terrain-provider-arcgis ref=\"provider\"></vc-terrain-provider-arcgis>\n  </vc-viewer>\n  <div class=\"demo-toolbar\">\n    <el-row>\n      <el-button type=\"danger\" round @click=\"unload\">销毁</el-button>\n      <el-button type=\"danger\" round @click=\"load\">加载</el-button>\n      <el-button type=\"danger\" round @click=\"reload\">重载</el-button>\n    </el-row>\n  </div>\n</el-row>\n\n<script>\n  import { ref, getCurrentInstance } from 'vue'\n  export default {\n    setup() {\n      // state\n      const instance = getCurrentInstance()\n      const provider = ref(null)\n      const imageryProvider = ref(null)\n      let viewer = undefined\n      // methods\n      const unload = () => {\n        provider.value.unload()\n      }\n      const reload = () => {\n        provider.value.reload()\n      }\n      const load = () => {\n        provider.value.load()\n      }\n      const onViewerReady = ({ Cesium, viewer }) => {\n        imageryProvider.value = new Cesium.ArcGisMapServerImageryProvider({\n          url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'\n        })\n        var target = new Cesium.Cartesian3(300770.50872389384, 5634912.131394585, 2978152.2865545116)\n        var offset = new Cesium.Cartesian3(6344.974098678562, -793.3419798081741, 2499.9508860763162)\n        viewer.camera.lookAt(target, offset)\n        viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY)\n      }\n      return {\n        provider,\n        unload,\n        reload,\n        load,\n        imageryProvider,\n        onViewerReady\n      }\n    }\n  }\n</script>\n")], -1);
+
+const _hoisted_8 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("属性 ");
+
+const _hoisted_9 = /*#__PURE__*/Object(vue_esm_browser["createStaticVNode"])("<table><thead><tr><th>Name</th><th>Type</th><th>Default</th><th>Description</th></tr></thead><tbody><tr><td>url</td><td>string | Cesium.Resource</td><td><code>&#39;https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer&#39;</code></td><td><code>required</code> 指定服务地址。</td></tr><tr><td>token</td><td>string</td><td></td><td><code>optional</code> 指定服务授权令牌。</td></tr><tr><td>ellipsoid</td><td>Cesium.Ellipsoid</td><td></td><td><code>optional</code> 指定参考椭球体。</td></tr></tbody></table>", 1);
+
+const _hoisted_10 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("事件 ");
+
+const _hoisted_11 = /*#__PURE__*/Object(vue_esm_browser["createStaticVNode"])("<table><thead><tr><th>事件名</th><th>参数</th><th>描述</th></tr></thead><tbody><tr><td>beforeLoad</td><td>(instance: VcComponentInternalInstance)</td><td>对象加载前触发。</td></tr><tr><td>ready</td><td>(readyObj: VcReadyObject)</td><td>对象加载成功时触发。</td></tr><tr><td>destroyed</td><td>(instance: VcComponentInternalInstance)</td><td>对象销毁时触发。</td></tr><tr><td>errorEvent</td><td>(evt: Cesium.TileProviderError)</td><td>当图层提供者发生异步错误时触发, 返回一个 TileProviderError 实例。</td></tr><tr><td>readyPromise</td><td>(evt: boolean )</td><td>当 provider 准备好使用时触发。</td></tr></tbody></table>", 1);
+
+const _hoisted_12 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("参考 ");
+
+const _hoisted_13 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("官方文档： ");
+
+const _hoisted_14 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("ArcGISTiledElevationTerrainProvider");
+
+function vc_terrain_provider_arcgisvue_type_template_id_da1455e6_render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_app_link = Object(vue_esm_browser["resolveComponent"])("app-link");
+
+  const _component_app_heading = Object(vue_esm_browser["resolveComponent"])("app-heading");
+
+  const _component_vue_cesium_demo0 = Object(vue_esm_browser["resolveComponent"])("vue-cesium-demo0");
+
+  const _component_demo_block = Object(vue_esm_browser["resolveComponent"])("demo-block");
+
+  const _component_right_nav = Object(vue_esm_browser["resolveComponent"])("right-nav");
+
+  return Object(vue_esm_browser["openBlock"])(), Object(vue_esm_browser["createElementBlock"])("section", vc_terrain_provider_arcgisvue_type_template_id_da1455e6_hoisted_1, [Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "vcterrainproviderarcgis",
+    tabindex: "-1",
+    content: "VcTerrainProviderArcgis",
+    href: "#vcterrainproviderarcgis",
+    level: "2"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [vc_terrain_provider_arcgisvue_type_template_id_da1455e6_hoisted_2, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#vcterrainproviderarcgis"
+    })]),
+    _: 1
+  }), vc_terrain_provider_arcgisvue_type_template_id_da1455e6_hoisted_3, Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "ji-chu-yong-fa",
+    tabindex: "-1",
+    content: "基础用法",
+    href: "#ji-chu-yong-fa",
+    level: "3"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [vc_terrain_provider_arcgisvue_type_template_id_da1455e6_hoisted_4, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#ji-chu-yong-fa"
+    })]),
+    _: 1
+  }), _hoisted_5, Object(vue_esm_browser["createVNode"])(_component_demo_block, null, {
+    source: Object(vue_esm_browser["withCtx"])(() => [Object(vue_esm_browser["createVNode"])(_component_vue_cesium_demo0)]),
+    highlight: Object(vue_esm_browser["withCtx"])(() => [_hoisted_7]),
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_6]),
+    _: 1
+  }), Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "shu-xing",
+    tabindex: "-1",
+    content: "属性",
+    href: "#shu-xing",
+    level: "3"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_8, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#shu-xing"
+    })]),
+    _: 1
+  }), _hoisted_9, Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "shi-jian",
+    tabindex: "-1",
+    content: "事件",
+    href: "#shi-jian",
+    level: "3"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_10, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#shi-jian"
+    })]),
+    _: 1
+  }), _hoisted_11, Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "can-kao",
+    tabindex: "-1",
+    content: "参考",
+    href: "#can-kao",
+    level: "3"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_12, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#can-kao"
+    })]),
+    _: 1
+  }), Object(vue_esm_browser["createElementVNode"])("ul", null, [Object(vue_esm_browser["createElementVNode"])("li", null, [_hoisted_13, Object(vue_esm_browser["createElementVNode"])("strong", null, [Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://cesium.com/docs/cesiumjs-ref-doc/ArcGISTiledElevationTerrainProvider.html"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_14]),
+    _: 1
+  })])])]), Object(vue_esm_browser["createVNode"])(_component_right_nav)]);
+}
+// CONCATENATED MODULE: ./website/docs/zh-CN/providers/vc-terrain-provider-arcgis.md?vue&type=template&id=da1455e6
+
+// CONCATENATED MODULE: ./node_modules/.pnpm/babel-loader@8.2.3_xtlouzbwsjm2tieu75n2ogelwy/node_modules/babel-loader/lib!./node_modules/.pnpm/vue-loader@16.5.0_fitqpr7w7mry5zwvh45opzxdee/node_modules/vue-loader/dist??ref--3-0!./website/md-loader!./website/docs/zh-CN/providers/vc-terrain-provider-arcgis.md?vue&type=script&lang=ts
+
+/* harmony default export */ var vc_terrain_provider_arcgisvue_type_script_lang_ts = ({
+  name: 'component-doc',
+  components: {
+    "vue-cesium-demo0": function () {
+      const {
+        resolveComponent: _resolveComponent,
+        createVNode: _createVNode,
+        withCtx: _withCtx,
+        createTextVNode: _createTextVNode,
+        createElementVNode: _createElementVNode,
+        openBlock: _openBlock,
+        createElementBlock: _createElementBlock
+      } = vue_esm_browser;
+      const _hoisted_1 = {
+        class: "demo-toolbar"
+      };
+
+      const _hoisted_2 = /*#__PURE__*/_createTextVNode("销毁");
+
+      const _hoisted_3 = /*#__PURE__*/_createTextVNode("加载");
+
+      const _hoisted_4 = /*#__PURE__*/_createTextVNode("重载");
+
+      function render(_ctx, _cache) {
+        const _component_vc_terrain_provider_arcgis = _resolveComponent("vc-terrain-provider-arcgis");
+
+        const _component_vc_viewer = _resolveComponent("vc-viewer");
+
+        const _component_el_button = _resolveComponent("el-button");
+
+        const _component_el_row = _resolveComponent("el-row");
+
+        return _openBlock(), _createElementBlock("div", null, [_createVNode(_component_el_row, {
+          ref: "viewerContainer",
+          class: "demo-viewer"
+        }, {
+          default: _withCtx(() => [_createVNode(_component_vc_viewer, {
+            onReady: _ctx.onViewerReady,
+            imageryProvider: _ctx.imageryProvider
+          }, {
+            default: _withCtx(() => [_createVNode(_component_vc_terrain_provider_arcgis, {
+              ref: "provider"
+            }, null, 512)]),
+            _: 1
+          }, 8, ["onReady", "imageryProvider"]), _createElementVNode("div", _hoisted_1, [_createVNode(_component_el_row, null, {
+            default: _withCtx(() => [_createVNode(_component_el_button, {
+              type: "danger",
+              round: "",
+              onClick: _ctx.unload
+            }, {
+              default: _withCtx(() => [_hoisted_2]),
+              _: 1
+            }, 8, ["onClick"]), _createVNode(_component_el_button, {
+              type: "danger",
+              round: "",
+              onClick: _ctx.load
+            }, {
+              default: _withCtx(() => [_hoisted_3]),
+              _: 1
+            }, 8, ["onClick"]), _createVNode(_component_el_button, {
+              type: "danger",
+              round: "",
+              onClick: _ctx.reload
+            }, {
+              default: _withCtx(() => [_hoisted_4]),
+              _: 1
+            }, 8, ["onClick"])]),
+            _: 1
+          })])]),
+          _: 1
+        }, 512)]);
+      }
+
+      const {
+        ref,
+        getCurrentInstance
+      } = vue_esm_browser;
+      const democomponentExport = {
+        setup() {
+          // state
+          const instance = getCurrentInstance();
+          const provider = ref(null);
+          const imageryProvider = ref(null);
+          let viewer = undefined; // methods
+
+          const unload = () => {
+            provider.value.unload();
+          };
+
+          const reload = () => {
+            provider.value.reload();
+          };
+
+          const load = () => {
+            provider.value.load();
+          };
+
+          const onViewerReady = _ref => {
+            let {
+              Cesium,
+              viewer
+            } = _ref;
+            imageryProvider.value = new Cesium.ArcGisMapServerImageryProvider({
+              url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer'
+            });
+            var target = new Cesium.Cartesian3(300770.50872389384, 5634912.131394585, 2978152.2865545116);
+            var offset = new Cesium.Cartesian3(6344.974098678562, -793.3419798081741, 2499.9508860763162);
+            viewer.camera.lookAt(target, offset);
+            viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
+          };
+
+          return {
+            provider,
+            unload,
+            reload,
+            load,
+            imageryProvider,
+            onViewerReady
+          };
+        }
+
+      };
+      return {
+        render,
+        ...democomponentExport
+      };
+    }()
+  }
+});
+// CONCATENATED MODULE: ./website/docs/zh-CN/providers/vc-terrain-provider-arcgis.md?vue&type=script&lang=ts
+ 
+// CONCATENATED MODULE: ./website/docs/zh-CN/providers/vc-terrain-provider-arcgis.md
+
+
+
+vc_terrain_provider_arcgisvue_type_script_lang_ts.render = vc_terrain_provider_arcgisvue_type_template_id_da1455e6_render
+
+/* harmony default export */ var vc_terrain_provider_arcgis = __webpack_exports__["default"] = (vc_terrain_provider_arcgisvue_type_script_lang_ts);
+
+/***/ })
+
+}]);
