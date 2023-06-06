2b03366d85aadce80bfac314b3c972b0e16fc032
zouyaoji <370681295@qq.com>
2023-06-06 17:48:11 +0000

    website deploy

---
+++ DOCS zh-CN50.54eb01249c308ef63599.js
@@ -0,0 +1,340 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[152],{
+
+/***/ 828:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: ./node_modules/.pnpm/vue@3.2.47/node_modules/vue/dist/vue.esm-browser.js
+var vue_esm_browser = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./node_modules/.pnpm/babel-loader@8.2.3_@babel+core@7.16.0_webpack@4.44.1/node_modules/babel-loader/lib!./node_modules/.pnpm/vue-loader@16.5.0_@vue+compiler-sfc@3.2.30_webpack@4.44.1/node_modules/vue-loader/dist/templateLoader.js??ref--6!./node_modules/.pnpm/vue-loader@16.5.0_@vue+compiler-sfc@3.2.30_webpack@4.44.1/node_modules/vue-loader/dist??ref--3-0!./website/md-loader!./website/docs/zh-CN/installation.md?vue&type=template&id=0ee932d0
+
+const _hoisted_1 = {
+  class: "content vue-cesium-doc"
+};
+
+const _hoisted_2 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("环境支持");
+
+const _hoisted_3 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("Vue for Cesium 可以在支持 ");
+
+const _hoisted_4 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("ES2018");
+
+const _hoisted_5 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 和 ");
+
+const _hoisted_6 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("WebGL");
+
+const _hoisted_7 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 的浏览器上运行。 如果您确实需要支持旧版本的浏览器，请自行添加 Babel 和相应的 Polyfill.");
+
+const _hoisted_8 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("p", null, "由于 Vue3 及 CesiumJS 1.85+ 不再支持 IE11，所以 Vue for Cesium 也不再支持 IE 浏览器。", -1);
+
+const _hoisted_9 = {
+  style: {
+    "text-align": "center"
+  }
+};
+const _hoisted_10 = {
+  style: {
+    "text-align": "center"
+  }
+};
+const _hoisted_11 = {
+  style: {
+    "text-align": "center"
+  }
+};
+const _hoisted_12 = {
+  style: {
+    "text-align": "center"
+  }
+};
+
+const _hoisted_13 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("tbody", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("tr", null, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", {
+  style: {
+    "text-align": "center"
+  }
+}, "Edge ≥ 79"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", {
+  style: {
+    "text-align": "center"
+  }
+}, "Firefox ≥ 78"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", {
+  style: {
+    "text-align": "center"
+  }
+}, "Chrome ≥ 64"), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("td", {
+  style: {
+    "text-align": "center"
+  }
+}, "Safari ≥ 12")])], -1);
+
+const _hoisted_14 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("安装");
+
+const _hoisted_15 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("使用包管理器安装 ");
+
+const _hoisted_16 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("我们建议您");
+
+const _hoisted_17 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("span", {
+  style: {
+    "color": "rgb(66 184 131)"
+  }
+}, [/*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("b", null, "使用包管理器")], -1);
+
+const _hoisted_18 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("（npm，");
+
+const _hoisted_19 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("yarn");
+
+const _hoisted_20 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("，");
+
+const _hoisted_21 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("pnpm");
+
+const _hoisted_22 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("）安装 ");
+
+const _hoisted_23 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("code", null, "Vue for Cesium", -1);
+
+const _hoisted_24 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("，然后您就可以使用打包工具，例如 ");
+
+const _hoisted_25 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("vite");
+
+const _hoisted_26 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(", ");
+
+const _hoisted_27 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("webpack");
+
+const _hoisted_28 = /*#__PURE__*/Object(vue_esm_browser["createStaticVNode"])("<pre class=\"example-code\"><code class=\"hljs language-shell\"><span class=\"hljs-meta\"># </span><span class=\"language-bash\">选择一个你喜欢的包管理器</span>\n<span class=\"hljs-meta\">\n# </span><span class=\"language-bash\">npm</span>\n<span class=\"hljs-meta\">$ </span><span class=\"language-bash\">npm install vue-cesium --save</span>\n<span class=\"hljs-meta\">\n# </span><span class=\"language-bash\">yarn</span>\n<span class=\"hljs-meta\">$ </span><span class=\"language-bash\">yarn add vue-cesium</span>\n<span class=\"hljs-meta\">\n# </span><span class=\"language-bash\">pnpm</span>\n<span class=\"hljs-meta\">$ </span><span class=\"language-bash\">pnpm add vue-cesium</span>\n</code><span class=\"lang-mark\">sh</span></pre><p>如果您的网络环境不好，建议使用 <code>nrm</code> 切换所用的包管理器的资源地址，或直接手动更改。</p>", 2);
+
+const _hoisted_30 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("如果是通过包管理器安装，并希望配合打包工具使用，请阅读下一节：");
+
+const _hoisted_31 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("快速上手");
+
+const _hoisted_32 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("。");
+
+const _hoisted_33 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("CDN 引入 ");
+
+const _hoisted_34 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("p", null, [/*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("Vue for Cesium 支持 CDN 方式引入 Vue for Cesium，这样在 window 上下文中就可以使用 "), /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("code", null, "Vue for Cesium"), /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 了。")], -1);
+
+const _hoisted_35 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("不同的 CDN 提供商有不同的引入方式，我们在这里以 ");
+
+const _hoisted_36 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("unpkg");
+
+const _hoisted_37 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 和 ");
+
+const _hoisted_38 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("jsdelivr");
+
+const _hoisted_39 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 举例，你也可以使用其它的 CDN 供应商。");
+
+const _hoisted_40 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("p", null, "如果你的项目使用环境相对封闭，你可以在部署的环境中自己配置 CesiumJS 并引入。", -1);
+
+const _hoisted_41 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("使用 unpkg ");
+
+const _hoisted_42 = /*#__PURE__*/Object(vue_esm_browser["createStaticVNode"])("<pre class=\"example-code\"><code class=\"hljs language-html\"><span class=\"hljs-tag\">&lt;<span class=\"hljs-name\">head</span>&gt;</span>\n  <span class=\"hljs-comment\">&lt;!-- 引入样式 --&gt;</span>\n  <span class=\"hljs-tag\">&lt;<span class=\"hljs-name\">link</span> <span class=\"hljs-attr\">rel</span>=<span class=\"hljs-string\">&quot;stylesheet&quot;</span> <span class=\"hljs-attr\">href</span>=<span class=\"hljs-string\">&quot;//unpkg.com/vue-cesium/dist/index.css&quot;</span> /&gt;</span>\n  <span class=\"hljs-comment\">&lt;!-- 引入 Vue3 --&gt;</span>\n  <span class=\"hljs-tag\">&lt;<span class=\"hljs-name\">script</span> <span class=\"hljs-attr\">src</span>=<span class=\"hljs-string\">&quot;//unpkg.com/vue&quot;</span>&gt;</span><span class=\"hljs-tag\">&lt;/<span class=\"hljs-name\">script</span>&gt;</span>\n  <span class=\"hljs-comment\">&lt;!-- 引入组件库 --&gt;</span>\n  <span class=\"hljs-tag\">&lt;<span class=\"hljs-name\">script</span> <span class=\"hljs-attr\">src</span>=<span class=\"hljs-string\">&quot;//unpkg.com/vue-cesium&quot;</span>&gt;</span><span class=\"hljs-tag\">&lt;/<span class=\"hljs-name\">script</span>&gt;</span>\n<span class=\"hljs-tag\">&lt;/<span class=\"hljs-name\">head</span>&gt;</span>\n</code><span class=\"lang-mark\">html</span></pre>", 1);
+
+const _hoisted_43 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("使用 jsDelivr ");
+
+const _hoisted_44 = /*#__PURE__*/Object(vue_esm_browser["createStaticVNode"])("<pre class=\"example-code\"><code class=\"hljs language-html\"><span class=\"hljs-tag\">&lt;<span class=\"hljs-name\">head</span>&gt;</span>\n  <span class=\"hljs-comment\">&lt;!-- 引入样式 --&gt;</span>\n  <span class=\"hljs-tag\">&lt;<span class=\"hljs-name\">link</span> <span class=\"hljs-attr\">rel</span>=<span class=\"hljs-string\">&quot;stylesheet&quot;</span> <span class=\"hljs-attr\">href</span>=<span class=\"hljs-string\">&quot;//cdn.jsdelivr.net/npm/vue-cesium/dist/index.css&quot;</span> /&gt;</span>\n  <span class=\"hljs-comment\">&lt;!-- 引入 Vue --&gt;</span>\n  <span class=\"hljs-tag\">&lt;<span class=\"hljs-name\">script</span> <span class=\"hljs-attr\">src</span>=<span class=\"hljs-string\">&quot;//cdn.jsdelivr.net/npm/vue&quot;</span>&gt;</span><span class=\"hljs-tag\">&lt;/<span class=\"hljs-name\">script</span>&gt;</span>\n  <span class=\"hljs-comment\">&lt;!-- 引入组件库 --&gt;</span>\n  <span class=\"hljs-tag\">&lt;<span class=\"hljs-name\">script</span> <span class=\"hljs-attr\">src</span>=<span class=\"hljs-string\">&quot;//cdn.jsdelivr.net/npm/vue-cesium@next&quot;</span>&gt;</span><span class=\"hljs-tag\">&lt;/<span class=\"hljs-name\">script</span>&gt;</span>\n<span class=\"hljs-tag\">&lt;/<span class=\"hljs-name\">head</span>&gt;</span>\n</code><span class=\"lang-mark\">html</span></pre>", 1);
+
+const _hoisted_45 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("如果你使用 CDN 引入 ");
+
+const _hoisted_46 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("code", null, "Vue for Cesium", -1);
+
+const _hoisted_47 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" ，我们建议在链接地址上锁定版本，以免将来 ");
+
+const _hoisted_48 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("code", null, "Vue for Cesium", -1);
+
+const _hoisted_49 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 升级时受到非兼容性更新的影响。锁定版本的方法请查看 ");
+
+const _hoisted_50 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("unpkg.com");
+
+const _hoisted_51 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 官方资料。");
+
+const _hoisted_52 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("Hello world");
+
+const _hoisted_53 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("通过 ");
+
+const _hoisted_54 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("strong", null, "CDN", -1);
+
+const _hoisted_55 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 的方式我们可以很容易地使用 ");
+
+const _hoisted_56 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("code", null, "Vue for Cesium", -1);
+
+const _hoisted_57 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])(" 写出一个 Hello world 页面。");
+
+const _hoisted_58 = /*#__PURE__*/Object(vue_esm_browser["createTextVNode"])("在线演示");
+
+const _hoisted_59 = /*#__PURE__*/Object(vue_esm_browser["createElementVNode"])("iframe", {
+  height: "500",
+  style: {
+    "width": "100%"
+  },
+  scrolling: "no",
+  title: "Vue for Cesium Demo",
+  src: "https://codepen.io/zouyaoji/embed/bGBOyJM?height=265&theme-id=light&default-tab=html,result",
+  frameborder: "no",
+  loading: "lazy",
+  allowtransparency: "true",
+  allowfullscreen: "true"
+}, "\n  See the Pen <a href='https://codepen.io/zouyaoji/pen/bGBOyJM'>Vue for Cesium Demo</a> by zouyaoji\n  (<a href='https://codepen.io/zouyaoji'>@zouyaoji</a>) on <a href='https://codepen.io'>CodePen</a>.\n", -1);
+
+function render(_ctx, _cache) {
+  const _component_app_heading = Object(vue_esm_browser["resolveComponent"])("app-heading");
+
+  const _component_app_link = Object(vue_esm_browser["resolveComponent"])("app-link");
+
+  const _component_app_img = Object(vue_esm_browser["resolveComponent"])("app-img");
+
+  const _component_right_nav = Object(vue_esm_browser["resolveComponent"])("right-nav");
+
+  return Object(vue_esm_browser["openBlock"])(), Object(vue_esm_browser["createElementBlock"])("section", _hoisted_1, [Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    content: "环境支持",
+    href: "",
+    level: "1"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_2]),
+    _: 1
+  }), Object(vue_esm_browser["createElementVNode"])("p", null, [_hoisted_3, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://caniuse.com/?feats=mdn-javascript_builtins_regexp_dotall,mdn-javascript_builtins_regexp_lookbehind_assertion,mdn-javascript_builtins_regexp_named_capture_groups,mdn-javascript_builtins_regexp_property_escapes,mdn-javascript_builtins_symbol_asynciterator,mdn-javascript_functions_method_definitions_async_generator_methods,mdn-javascript_grammar_template_literals_template_literal_revision,mdn-javascript_operators_destructuring_rest_in_objects,mdn-javascript_operators_spread_spread_in_destructuring,promise-finally"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_4]),
+    _: 1
+  }), _hoisted_5, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://caniuse.com/webgl"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_6]),
+    _: 1
+  }), _hoisted_7]), _hoisted_8, Object(vue_esm_browser["createElementVNode"])("table", null, [Object(vue_esm_browser["createElementVNode"])("thead", null, [Object(vue_esm_browser["createElementVNode"])("tr", null, [Object(vue_esm_browser["createElementVNode"])("th", _hoisted_9, [Object(vue_esm_browser["createElementVNode"])("div", null, [Object(vue_esm_browser["createVNode"])(_component_app_img, {
+    src: "https://unpkg.com/@browser-logos/edge/edge_32x32.png",
+    alt: "edge",
+    title: "null"
+  })])]), Object(vue_esm_browser["createElementVNode"])("th", _hoisted_10, [Object(vue_esm_browser["createElementVNode"])("div", null, [Object(vue_esm_browser["createVNode"])(_component_app_img, {
+    src: "https://unpkg.com/@browser-logos/firefox/firefox_32x32.png",
+    alt: "Firefox",
+    title: "null"
+  })])]), Object(vue_esm_browser["createElementVNode"])("th", _hoisted_11, [Object(vue_esm_browser["createElementVNode"])("div", null, [Object(vue_esm_browser["createVNode"])(_component_app_img, {
+    src: "https://unpkg.com/@browser-logos/chrome/chrome_32x32.png",
+    alt: "Chrome",
+    title: "null"
+  })])]), Object(vue_esm_browser["createElementVNode"])("th", _hoisted_12, [Object(vue_esm_browser["createElementVNode"])("div", null, [Object(vue_esm_browser["createVNode"])(_component_app_img, {
+    src: "https://unpkg.com/@browser-logos/safari/safari_32x32.png",
+    alt: "Safari",
+    title: "null"
+  })])])])]), _hoisted_13]), Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    content: "安装",
+    href: "",
+    level: "1"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_14]),
+    _: 1
+  }), Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "shi-yong-bao-guan-li-qi-an-zhuang",
+    tabindex: "-1",
+    content: "使用包管理器安装",
+    href: "#shi-yong-bao-guan-li-qi-an-zhuang",
+    level: "2"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_15, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#shi-yong-bao-guan-li-qi-an-zhuang"
+    })]),
+    _: 1
+  }), Object(vue_esm_browser["createElementVNode"])("p", null, [_hoisted_16, _hoisted_17, _hoisted_18, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://classic.yarnpkg.com/lang/en/"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_19]),
+    _: 1
+  }), _hoisted_20, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://pnpm.io/zh/"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_21]),
+    _: 1
+  }), _hoisted_22, _hoisted_23, _hoisted_24, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://vitejs.dev"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_25]),
+    _: 1
+  }), _hoisted_26, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://webpack.js.org/"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_27]),
+    _: 1
+  })]), _hoisted_28, Object(vue_esm_browser["createElementVNode"])("p", null, [_hoisted_30, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "./#/zh-CN/component/quickstart"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_31]),
+    _: 1
+  }), _hoisted_32]), Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "cdn-yin-ru",
+    tabindex: "-1",
+    content: "CDN 引入",
+    href: "#cdn-yin-ru",
+    level: "2"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_33, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#cdn-yin-ru"
+    })]),
+    _: 1
+  }), _hoisted_34, Object(vue_esm_browser["createElementVNode"])("p", null, [_hoisted_35, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://unpkg.com"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_36]),
+    _: 1
+  }), _hoisted_37, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://jsdelivr.com"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_38]),
+    _: 1
+  }), _hoisted_39]), _hoisted_40, Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "shi-yong-unpkg",
+    tabindex: "-1",
+    content: "使用 unpkg",
+    href: "#shi-yong-unpkg",
+    level: "3"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_41, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#shi-yong-unpkg"
+    })]),
+    _: 1
+  }), _hoisted_42, Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    id: "shi-yong-jsdelivr",
+    tabindex: "-1",
+    content: "使用 jsDelivr",
+    href: "#shi-yong-jsdelivr",
+    level: "3"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_43, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+      class: "header-anchor",
+      href: "#shi-yong-jsdelivr"
+    })]),
+    _: 1
+  }), _hoisted_44, Object(vue_esm_browser["createElementVNode"])("p", null, [_hoisted_45, _hoisted_46, _hoisted_47, _hoisted_48, _hoisted_49, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://unpkg.com"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_50]),
+    _: 1
+  }), _hoisted_51]), Object(vue_esm_browser["createVNode"])(_component_app_heading, {
+    content: "Hello world",
+    href: "",
+    level: "1"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_52]),
+    _: 1
+  }), Object(vue_esm_browser["createElementVNode"])("p", null, [_hoisted_53, _hoisted_54, _hoisted_55, _hoisted_56, _hoisted_57, Object(vue_esm_browser["createVNode"])(_component_app_link, {
+    href: "https://codepen.io/zouyaoji/pen/bGBOyJM"
+  }, {
+    default: Object(vue_esm_browser["withCtx"])(() => [_hoisted_58]),
+    _: 1
+  })]), _hoisted_59, Object(vue_esm_browser["createVNode"])(_component_right_nav)]);
+}
+// CONCATENATED MODULE: ./website/docs/zh-CN/installation.md?vue&type=template&id=0ee932d0
+
+// CONCATENATED MODULE: ./website/docs/zh-CN/installation.md
+
+const script = {}
+script.render = render
+
+/* harmony default export */ var installation = __webpack_exports__["default"] = (script);
+
+/***/ })
+
+}]);
