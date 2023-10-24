a25b3a9c9479215d2ab7146cd649d43bb87fdbd9
zouyaoji <370681295@qq.com>
2023-10-24 17:56:07 +0800

    chore(vue-cesium): :hammer: updated

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 /*
  * @Author: zouyaoji@https://github.com/zouyaoji
  * @Date: 2021-09-16 09:28:13
- * @LastEditTime: 2022-01-13 22:19:45
- * @LastEditors: zouyaoji
+ * @LastEditTime: 2023-10-24 15:26:22
+ * @LastEditors: zouyaoji 370681295@qq.com
  * @Description:
  * @FilePath: \vue-cesium@next\babel.config.js
  */
@@ -13,19 +13,39 @@ module.exports = {
   // See https://github.com/babel/babel/issues/12066
   presets: [
     [
-      '@babel/env',
+      '@babel/preset-env',
       {
         loose: false,
         modules: false
       }
     ],
-    '@babel/typescript'
+    '@babel/preset-typescript'
   ],
   plugins: [
-    '@vue/babel-plugin-jsx',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
+    // Stage 0
+    // '@babel/plugin-proposal-function-bind',
+    // Stage 1
+    // '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-logical-assignment-operators',
+    ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+    // ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+    ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
+    // '@babel/plugin-proposal-do-expressions',
+    // Stage 2
+    // ['@babel/plugin-proposal-decorators', { legacy: true }],
+    // '@babel/plugin-proposal-function-sent',
+    // '@babel/plugin-proposal-export-namespace-from',
+    // '@babel/plugin-proposal-numeric-separator',
+    // '@babel/plugin-proposal-throw-expressions',
+    // Stage 3
+    // '@babel/plugin-syntax-dynamic-import',
+    // '@babel/plugin-syntax-import-meta',
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
     ['@babel/plugin-proposal-private-methods', { loose: false }],
+    // '@babel/plugin-proposal-json-strings',
+
+    // other
+    '@vue/babel-plugin-jsx',
     '@babel/transform-runtime'
   ],
   overrides: [
@@ -39,7 +59,7 @@ module.exports = {
       ignore: ['**/*.test.ts', '**/*.spec.ts'],
       presets: [
         [
-          '@babel/env',
+          '@babel/preset-env',
           {
             loose: true,
             modules: false
