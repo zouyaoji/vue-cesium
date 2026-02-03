bbd202d97092db88a8d85f6fc40ce2b4a70f3950
zouyaoji <370681295@qq.com>
2026-02-04 01:00:56 +0800

    chore: 发测试包

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,10 @@
 ###
  # @Author: zouyaoji@https://github.com/zouyaoji
  # @Date: 2021-09-16 09:28:13
- # @LastEditTime: 2023-02-01 15:48:00
- # @LastEditors: zouyaoji
+ # @LastEditTime: 2026-02-04 00:59:42
+ # @LastEditors: zouyaoji 370681295@qq.com
  # @Description:
- # @FilePath: \vue-cesium@next\scripts\publish.sh
+ # @FilePath: \vue-cesium\scripts\publish.sh
 ###
 
 #!/bin/sh
@@ -17,7 +17,7 @@ pnpm update:version
 pnpm build
 
 cd dist/vue-cesium
-npm publish --tag latest --access public --registry ${REGISTRY}
+npm publish --tag beta --access public --registry ${REGISTRY}
 cd -
 
 echo "✅ Publish completed"
